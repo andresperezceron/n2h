8251451c601326732c6e3a1ed52f98b881a63e74
andresperezceron <aperezlevi@gmail.com>
2019-01-18 23:54:43 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ var Periodo = require("./Periodo.js");
 var result = document.getElementById("result");
 var numStr = document.getElementById("numStr");
 
-numStr.onkeypress = function botonOnKeyDown(event) {
-
+numStr.onkeypress = function botonOnKeyDown(e) {
+    result.innerText = e.keyCode;
     //if((event.key > '9' || event.key < '0') && event.key !== "Backspace")
       //  return false;
 };
 
-numStr.onkeyup = function botonOnClick() {
-    result.innerText = event.key;
+numStr.onkeyup = function botonOnClick(e) {
+    console.log("entron en okeyup")
     //result.innerText = new Periodo(numStr.value, "Spanish").handle();
 };
 
