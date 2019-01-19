28c0009ffb9b05b60776b7bcfa40fa7fdd91d5b1
andresperezceron <aperezlevi@gmail.com>
2019-01-19 02:26:47 +0100

    index.js

--- src/index.js
+++ src/index.js
@@ -5,20 +5,19 @@ var numStr = document.getElementById("numStr");
 
 numStr.onkeypress = function botonOnKeyDown(e) {
     var maxleng = 66;
-    if(numStr.value.length === maxleng)
+    /*if(numStr.value.length === maxleng)
         return false;
     if((e.key > '9' || e.key < '0') && e.key !== "Backspace")
-        return false;
+        return false;*/
 };
 
 numStr.onkeyup = function botonOnClick(e) {
     var maxleng = 66;
-
+    var validNumber = "";
     if(numStr.value.length > maxleng)
         numStr.value = numStr.value.substring(0, maxleng);
 
     if(invalidNumber(numStr.value)) {
-        var validNumber = "";
         var novalidNumber = numStr.value;
         for(var i = 0; i < novalidNumber.length; i++) {
             if(novalidNumber.charAt(i) >= '0' && novalidNumber.charAt(i) <= '9')
